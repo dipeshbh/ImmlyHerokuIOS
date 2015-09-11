493cfa6f8e0a1142a0d265f838c55680a7c15964
dipeshbh <i836816@MacBook-Pro.local>
2015-09-10 23:59:27 -0700

    Initial Commit

---
+++ ImmlyHeroku/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ImmlyHeroku
+//
+//  Created by Bhattacharya, Dipesh on 9/10/15.
+//  Copyright (c) 2015 Immly. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
